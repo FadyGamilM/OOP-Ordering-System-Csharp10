ba3e577ce8897a3bdc0d6c0359461f2092842206
fady <gamilfady605@gmail.com>
2023-04-05 19:55:40 +0200

    override the ToString method for each order-related class

--- Domain/Order/Order.cs
+++ Domain/Order/Order.cs
@@ -1,3 +1,4 @@
+using System.Text;
 namespace BakeryInventorySystem.Domain.Order;
 
 public class Order
@@ -5,6 +6,24 @@ public class Order
    public int Id { get; set; }
    public List<OrderItem> OrderedItems { get; set; } = new List<OrderItem> { };
    public DateOnly OrderedAt { get; set; }
+   public DateOnly FullfillmentDate { get; set; }
    public bool IsFullfilled = false;
+
+   public Order()
+   {
+      // random id 
+      this.Id = new Random().Next(1000_000_000);
+
+      // random fullfillment date 
+      this.FullfillmentDate.AddDays(new Random().Next(10));
+   }
+
+   public override string ToString()
+   {
+      var sb = new StringBuilder { };
+      sb.Append($"Order Number {this.Id}");
+      sb.Append($"Number Of Ordered Items Of This Order {this.OrderedItems.Count}");
+      return sb.ToString();
+   }
 
 }
--- Domain/Order/OrderItem.cs
+++ Domain/Order/OrderItem.cs
@@ -5,4 +5,9 @@ public class OrderItem
    public int Id { get; set; }
    public int OrderedAmount { get; set; }
    public int ItemId { get; set; }
+
+   public override string ToString()
+   {
+      return $"The Ordered Item Has Id = {this.Id}, and the ordered amount from this item is = {this.OrderedAmount}";
+   }
 }
